459f282b12d29a00c18fd2f83fd1ab6782edeae4
Katatsumuri.pan <Katatsumuri.pan@gmail.com>
2024-03-05 19:12:48 +0900

    Add check icon(already exists in the .png)

--- src/main/java/betterquesting/api2/client/gui/themes/presets/PresetIcon.java
+++ src/main/java/betterquesting/api2/client/gui/themes/presets/PresetIcon.java
@@ -99,7 +99,8 @@ public enum PresetIcon {
     ICON_MENU("icon_menu"),
 
     ICON_PATREON("icon_patreon"),
-    ICON_TWITCH("icon_twitch");
+    ICON_TWITCH("icon_twitch"),
+    ICON_CHECK("icon_check");
 
     public static final ResourceLocation TX_ICONS = new ResourceLocation(ModReference.MODID, "textures/gui/editor_icons.png");
 
@@ -197,5 +198,6 @@ public enum PresetIcon {
 
         reg.setDefaultTexture(ICON_PATREON.key, new SimpleTexture(TX_ICONS, new GuiRectangle(144, 80, 16, 16)).maintainAspect(true));
         reg.setDefaultTexture(ICON_TWITCH.key, new SimpleTexture(TX_ICONS, new GuiRectangle(160, 80, 16, 16)).maintainAspect(true));
+        reg.setDefaultTexture(ICON_CHECK.key, new SimpleTexture(TX_ICONS, new GuiRectangle(0, 48, 16, 16)).maintainAspect(true));
     }
 }
