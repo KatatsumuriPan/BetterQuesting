b5b82b873f7f59c4cdc91307cf6db3be2e1a24e5
KatatsumuriPan <Katatsumuri.pan@gmail.com>
2024-03-13 17:40:29 +0900

    spotless

--- src/main/java/betterquesting/core/ModReference.java
+++ src/main/java/betterquesting/core/ModReference.java
@@ -3,7 +3,9 @@ package betterquesting.core;
 import betterquesting.Tags;
 
 public class ModReference {
-	public static final String MODID = Tags.ID;
-	public static final String NAME = Tags.NAME;
-	public static final String VERSION = Tags.VERSION;
+
+    public static final String MODID = Tags.ID;
+    public static final String NAME = Tags.NAME;
+    public static final String VERSION = Tags.VERSION;
+
 }
