318c105818ffa5a23d0e18be8cfba30bc24643d9
KatatsumuriPan <Katatsumuri.pan@gmail.com>
2024-03-14 22:33:18 +0900

    abstract database

--- src/main/java/betterquesting/api2/storage/ArrayCacheLookupLogic.java
+++ src/main/java/betterquesting/api2/storage/ArrayCacheLookupLogic.java
@@ -8,8 +8,8 @@ class ArrayCacheLookupLogic<T> extends LookupLogic<T> {
     private DBEntry<T>[] cache = null;
     private int offset = -1;
 
-    public ArrayCacheLookupLogic(SimpleDatabase<T> simpleDatabase) {
-        super(simpleDatabase);
+    public ArrayCacheLookupLogic(AbstractDatabase<T> abstractDatabase) {
+        super(abstractDatabase);
     }
 
     @Override
@@ -21,7 +21,8 @@ class ArrayCacheLookupLogic<T> extends LookupLogic<T> {
 
     @Override
     public List<DBEntry<T>> getRefCache() {
-        if (refCache != null) return refCache;
+        if (refCache != null)
+            return refCache;
         if (cache == null) {
             return super.getRefCache();
         } else {
@@ -35,7 +36,8 @@ class ArrayCacheLookupLogic<T> extends LookupLogic<T> {
         computeCache();
         List<DBEntry<T>> list = new ArrayList<>(keys.length);
         for (int k : keys) {
-            if (k - offset >= cache.length) continue;
+            if (k - offset >= cache.length)
+                continue;
             final DBEntry<T> element = cache[k - offset];
             if (element != null) {
                 // it shouldn't place too much allocation/gc pressure since there aren't too many keys to look up anyway
@@ -47,11 +49,12 @@ class ArrayCacheLookupLogic<T> extends LookupLogic<T> {
 
     @SuppressWarnings("unchecked")
     private void computeCache() {
-        if (cache != null) return;
-        cache = new DBEntry[simpleDatabase.mapDB.lastKey() - simpleDatabase.mapDB.firstKey() + 1];
-        offset = simpleDatabase.mapDB.firstKey();
+        if (cache != null)
+            return;
+        cache = new DBEntry[abstractDatabase.mapDB.lastKey() - abstractDatabase.mapDB.firstKey() + 1];
+        offset = abstractDatabase.mapDB.firstKey();
         if (refCache == null) {
-            for (Map.Entry<Integer, T> entry : simpleDatabase.mapDB.entrySet()) {
+            for (Map.Entry<Integer, T> entry : abstractDatabase.mapDB.entrySet()) {
                 cache[entry.getKey() - offset] = new DBEntry<>(entry.getKey(), entry.getValue());
             }
         } else {
